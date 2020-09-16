1772db8baff748c1c3caebc072f9c86b680ce13b
Gdeft <63198340+Gdeft@users.noreply.github.com>
2020-09-16 22:02:56 +0700

    1.3: Port to 6.0 with broken shader

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,43 @@
-if(!Vars.headless){
-	importPackage(Packages.arc.graphics.gl);
-	const shader = new JavaAdapter(Shader, {
-		apply(){
-			this.setUniformf("u_time", Time.time() / Scl.scl(1.0));
-		}
-	},
-    "uniform mat4 u_projTrans;attribute vec4 a_position;attribute vec2 a_texCoord0;attribute vec4 a_color;varying vec4 v_color;varying vec2 v_texCoord;void main(){gl_Position = u_projTrans * a_position;v_texCoord = a_texCoord0;v_color = a_color;}", "#ifdef GL_ES\nprecision mediump float;precision mediump int;\n#endif\nuniform sampler2D u_texture;uniform float u_time;varying vec4 v_color;varying vec2 v_texCoord;void main(){vec4 color = texture2D(u_texture, v_texCoord.xy);float t = clamp((sin(u_time * .02 + gl_FragCoord.x * .01 + gl_FragCoord.y * .005) + 1.) / 2., 0., 1.);vec3 c = vec3(mix(0., 1., t), mix(.89, .39, t), mix(1., .85, t));gl_FragColor = vec4(color.rgb * c.rgb, mix(.10, 1., t));}");
-}
+const music = loadMusic("Bach - Fugue in D Minor");
+var buffer, shader;
 
-Draw.shader(shader);
-Draw.blend(Blending.additive);
+const beginDraw = () => {
+	if(!buffer){
+		buffer = new FrameBuffer(Core.graphics.getWidth(), Core.graphics.getHeight());
+	};
 
-const arr = Object.keys(Pal);
+	buffer.resize(Core.graphics.getWidth(), Core.graphics.getHeight());
+	buffer.begin(Color.clear);
+};
 
-Events.on(EventType.Trigger.update, run(()=>{
-    for(var i = 0; i < arr.length; i++){
-        try{
-            Pal[arr[i]] = Pal[arr[i]].shiftHue(0.2);
-        }
-        catch(err){}
-    }
-}));
+const endDraw = () => {
+	buffer.end();
+
+	shader.bind();
+	shader.setUniformf("u_time", Time.globalTime() / Scl.scl(1.0));
+	Draw.blend(Blending.additive);
+	Draw.blit(buffer, shader);
+};
+
+Events.on(ClientLoadEvent, e => {
+	Vars.control.music = extend(MusicControl, {
+		update(){}
+	});
+
+	Vars.control.music.ambientMusic = Seq();
+	Vars.control.music.darkMusic = Seq();
+	music.setLooping(true);
+	music.play();
+
+	shader = new Shader(readString("shaders/bleach.vert"), readString("shaders/bleach.frag"));
+});
+
+Events.run(Trigger.preDraw, () => beginDraw());
+
+Events.run(Trigger.uiDrawBegin, () => {
+	if(Vars.state.isMenu()){
+		beginDraw();
+	};
+});
+
+Events.run(Trigger.uiDrawEnd, () => endDraw());
